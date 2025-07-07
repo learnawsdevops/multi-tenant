67464115c7a3d66fe5d1fa9885f1be487b741035
learnawsdevops <ramdevi0307@gmail.com>
2025-07-07 23:22:52 +0530

    change in tenants

--- tenants/tenant1/helm-install.sh
+++ tenants/tenant1/helm-install.sh
@@ -3,6 +3,6 @@ NAMESPACE=$TENANT_NAME
 
 kubectl create ns $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
-helm upgrade --install $TENANT_NAME ../../tenants/helm-chart  \
+helm upgrade --install $TENANT_NAME ../../helm-chart  \
   -n $NAMESPACE \
   -f values.yaml
